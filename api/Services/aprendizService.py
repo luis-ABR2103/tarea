1a1175e06d007582766bfeb907aaa3a8551c43ea
danna abreo <luisabreoangel123@gmail.com>
2026-09-07 23:51:02 -0500

    se agregaron los servicios

--- api/Services/aprendizService.py
+++ api/Services/aprendizService.py
@@ -18,6 +18,7 @@ class aprendizService:
         c  = current_app.mysql.connection.cursor()
         c.execute(sql)
         data = c.fetchall()
-        print(data)
-        # c.close()
-        return ""
+        data = [ Aprendiz(x[0], x[1], x[2], x[3]) for x in data]
+        c.close()
+        return data
+# relacionado todo lo q esta en la base de datos con la clase aprendiz
